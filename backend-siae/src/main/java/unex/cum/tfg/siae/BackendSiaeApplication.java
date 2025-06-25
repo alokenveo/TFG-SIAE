7b61282bcfcfc147cd7b16a002ddc7843bc924e3
alokenveo <alokenveo@alumnos.unex.es>
2025-06-25 19:19:42 +0200

    1ª subida del proyecto backend

---
+++ backend-siae/src/main/java/unex/cum/tfg/siae/BackendSiaeApplication.java
@@ -0,0 +1,13 @@
+package unex.cum.tfg.siae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSiaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSiaeApplication.class, args);
+	}
+
+}
---
+++ backend-siae/src/test/java/unex/cum/tfg/siae/BackendSiaeApplicationTests.java
@@ -0,0 +1,13 @@
+package unex.cum.tfg.siae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSiaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
